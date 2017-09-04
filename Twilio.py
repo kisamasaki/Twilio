3deb684be74d49911da1dbe0d4bafd5f4e17d0d8
kisamasaki <31547716+kisamasaki@users.noreply.github.com>
2017-09-04 21:39:44 +0900

    Create Twilio.py

---
+++ Twilio.py
@@ -0,0 +1,8 @@
+# Twiliofrom twilio.rest import Client
+account_sid = "##############################"
+auth_token = "##############################"
+client = Client(account_sid, auth_token)
+client.messages.create(
+    to="+12345678901",
+    from_="+819012345678",
+    body="退屈な作業が終わったよ")
